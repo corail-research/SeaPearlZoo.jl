5c8429085e78b6ddae4da330ed41c1748431dd5c
Ilan Coulon <ilan.coulon@gmail.com>
2020-12-25 13:25:16 +0100

    Use new EdgeFtLayer

--- learning_cp/tsptw/agents.jl
+++ learning_cp/tsptw/agents.jl
@@ -1,28 +1,55 @@
 nn_args = SeaPearl.ArgsVariableOutputGCN(numInFeatures = 6, state_rep=SeaPearl.TsptwStateRepresentation)
 
+numInFeatures = 6
+
 agent = RL.Agent(
     policy = RL.QBasedPolicy(
         learner = SeaPearl.CPDQNLearner(
             approximator = RL.NeuralNetworkApproximator(
-                model = SeaPearl.build_model(SeaPearl.VariableOutputGCN, nn_args),
-                optimizer = ADAM(0.001f0)
+                model = SeaPearl.FlexVariableOutputGNN(
+                    graphChain = Flux.Chain(
+                        SeaPearl.EdgeFtLayer(; v_dim=numInFeatures => 30, e_dim= 1 => 4),
+                        SeaPearl.EdgeFtLayer(; v_dim=30 => 20, e_dim= 4 => 4),
+                        SeaPearl.EdgeFtLayer(; v_dim=20 => 10, e_dim= 4 => 4),
+                    ),
+                    nodeChain = Flux.Chain(
+                        Flux.Dense(10, 10, relu),
+                        Flux.Dense(10, 10, relu),
+                        Flux.Dense(10, 10, relu),
+                    ),
+                    outputLayer = Flux.Dense(20, 1),
+                    state_rep=SeaPearl.TsptwStateRepresentation
+                ),
+                optimizer = ADAM(0.0001f0)
             ),
             target_approximator = RL.NeuralNetworkApproximator(
-                model = SeaPearl.build_model(SeaPearl.VariableOutputGCN, nn_args),
-                optimizer = ADAM(0.001f0)
+                model = SeaPearl.FlexVariableOutputGNN(
+                    graphChain = Flux.Chain(
+                        SeaPearl.EdgeFtLayer(; v_dim=numInFeatures => 30, e_dim= 1 => 4),
+                        SeaPearl.EdgeFtLayer(; v_dim=30 => 20, e_dim= 4 => 4),
+                        SeaPearl.EdgeFtLayer(; v_dim=20 => 10, e_dim= 4 => 4),
+                    ),
+                    nodeChain = Flux.Chain(
+                        Flux.Dense(10, 10, relu),
+                        Flux.Dense(10, 10, relu),
+                        Flux.Dense(10, 10, relu),
+                    ),
+                    outputLayer = Flux.Dense(20, 1),
+                    state_rep=SeaPearl.TsptwStateRepresentation
+                ),
+                optimizer = ADAM(0.0001f0)
             ),
             loss_func = huber_loss,
             stack_size = nothing,
-            γ = 0.9999f0,
-            batch_size = 1, #32,
-            update_horizon = 100,
+            γ = 0.99f0,
+            batch_size = 1,#32,
+            update_horizon = 1,
             min_replay_history = 1,
-            update_freq = 40,
-            target_update_freq = 150,
-            seed = 22,
+            update_freq = 1,
+            target_update_freq = 100,
         ), 
         explorer = SeaPearl.CPEpsilonGreedyExplorer(
-            ϵ_stable = 0.001,
+            ϵ_stable = 0.01,
             kind = :exp,
             ϵ_init = 1.0,
             warmup_steps = 0,
@@ -34,7 +61,7 @@ agent = RL.Agent(
         )
     ),
     trajectory = RL.CircularCompactSARTSATrajectory(
-        capacity = 1000, 
+        capacity = 2000, 
         state_type = Float32, 
         state_size = state_size,#(46, 93, 1),
         action_type = Int,
--- learning_cp/tsptw/tsptw.jl
+++ learning_cp/tsptw/tsptw.jl
@@ -3,6 +3,7 @@ using ReinforcementLearning
 const RL = ReinforcementLearning
 using Flux
 using GeometricFlux
+using Statistics
 
 using Plots
 gr()
@@ -11,34 +12,50 @@ gr()
 include("rewards.jl")
 ####
 
-n_city = 41
+n_city = 21
+# n_city = 11
 grid_size = 100
 max_tw_gap = 10
 max_tw = 100
 
 numInFeatures = 6
 
-tsptw_generator = SeaPearl.TsptwGenerator(n_city, grid_size, max_tw_gap, max_tw)
+tsptw_generator = SeaPearl.TsptwGenerator(n_city, grid_size, max_tw_gap, max_tw, true)
 
 # numberOfCPNodes = 1 + floor(Int64, coloring_generator.nb_nodes * ( 3 + coloring_generator.density ))
 #numberOfCPNodes = 141
 
 state_size = (n_city, n_city+6+2)
 
-include("agents.jl")
-learnedHeuristic = SeaPearl.LearnedHeuristic{SeaPearl.TsptwStateRepresentation, SeaPearl.TsptwReward, SeaPearl.VariableOutput}(agent)
-
+n_episodes = 3000
+n_trainings = 20
+loss = zeros(n_episodes)
+struct TsptwVariableSelection{TakeObjective} <: SeaPearl.AbstractVariableSelection{TakeObjective} end
+TsptwVariableSelection(;take_objective=false) = TsptwVariableSelection{take_objective}()
 selectMin(x::SeaPearl.IntVar) = SeaPearl.minimum(x.domain)
 heuristic_min = SeaPearl.BasicHeuristic(selectMin)
 
-struct TsptwVariableSelection{TakeObjective} <: SeaPearl.AbstractVariableSelection{TakeObjective} end
+# for _ in 1:n_trainings
+include("agents.jl")
+learnedHeuristic = SeaPearl.LearnedHeuristic{SeaPearl.TsptwStateRepresentation, SeaPearl.TsptwReward, SeaPearl.VariableOutput}(agent)
 
-TsptwVariableSelection(;take_objective=false) = TsptwVariableSelection{take_objective}()
 
 function (::TsptwVariableSelection{false})(cpmodel::SeaPearl.CPModel; rng=nothing)
     for i in 1:length(keys(cpmodel.variables))
+        # if haskey(cpmodel.variables, "a_"*string(i))
+        #     # println("cpmodel.variables[*string(i)]", cpmodel.variables["a_"*string(i)])
+        #     println(cpmodel.variables["a_"*string(i)])
+        # end
         if haskey(cpmodel.variables, "a_"*string(i)) && !SeaPearl.isbound(cpmodel.variables["a_"*string(i)])
+            # println("cpmodel.variables[*string(i)]", cpmodel.variables["a_"*string(i)])
             return cpmodel.variables["a_"*string(i)]
+        end
+    end
+    println("NO FREE VAR FOUND")
+    for i in 1:length(keys(cpmodel.variables))
+        if haskey(cpmodel.variables, "a_"*string(i))
+            # println("cpmodel.variables[*string(i)]", cpmodel.variables["a_"*string(i)])
+            println(cpmodel.variables["a_"*string(i)])
         end
     end
     println(cpmodel.variables)
@@ -46,28 +63,51 @@ end
 
 variableSelection = TsptwVariableSelection()
 
+
+maxNumberOfSteps = n_episodes
+loss = zeros(maxNumberOfSteps)
+rewards = zeros(maxNumberOfSteps)
+numberOfSteps = 1
+function metricsFun(;kwargs...)
+    if numberOfSteps < maxNumberOfSteps
+        loss[numberOfSteps] = kwargs[:loss]
+        rewards[numberOfSteps] = kwargs[:total_reward]
+        global numberOfSteps += 1
+    end
+end
+
 ############# TRAIN
 
-eval_freq = 1000
+eval_freq = 500
 
 bestsolutions, nodevisited, timeneeded, eval_nodes, eval_tim = SeaPearl.train!(
-    valueSelectionArray=[learnedHeuristic, heuristic_min], 
+    valueSelectionArray=[learnedHeuristic], 
     generator=tsptw_generator,
-    nb_episodes=3000,
+    nb_episodes=n_episodes,
     strategy=SeaPearl.DFSearch,
     variableHeuristic=variableSelection,
-    verbose = false,
     out_solver=true,
+    metricsFun=metricsFun,
+    verbose = false,
     evaluator=SeaPearl.SameInstancesEvaluator(; eval_freq = eval_freq, nb_instances = 5)
 )
 
+# ode_visited_delta += (nodevisited[:, 1]-nodevisited[:, 2])/n_trainings
+# end
+
 # plot 
-a, b = size(nodevisited)
-x = 1:a
-
-p1 = plot(x, nodevisited, xlabel="Episode", ylabel="Number of nodes visited", ylims = [0, 1000])
-
-p5 = plot(1:(floor(Int64, a/eval_freq)), eval_nodes[1:floor(Int64, a/eval_freq), :], xlabel="Evaluation", ylabel="Number of nodes visited", ylims = [0, 20000])
+# a = size(node_visited_delta, 1)
+x = 1:n_episodes
+mean_nodevisited = zeros(n_episodes)
+mean_reward = zeros(n_episodes)
+mean_over = 10
+for i in (mean_over+1):n_episodes
+    mean_nodevisited[i] = mean(nodevisited[(i-mean_over):i])
+    mean_reward[i] = mean(rewards[(i-mean_over):i])
+end
+p1 = plot(x, [mean_nodevisited .- 2, mean_reward], xlabel="Episode", ylabel="Reward/Steps", ylims = [0, 11])
+display(p1)
+p5 = plot(1:(floor(Int64, n_episodes/eval_freq)), eval_nodes[1:floor(Int64, n_episodes/eval_freq), :], xlabel="Evaluation", ylabel="Number of nodes visited", ylims = [0, 800])
 display(p5)
 
 ############# BENCHMARK
