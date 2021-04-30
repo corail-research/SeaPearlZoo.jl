b5d7d2e79ce8a3d1f31d570be40a630eb5bf7a45
tomsander1998 <tom.sander@polytechnique.edu>
2021-04-30 14:40:04 -0400

    Adding ValueSelection and Variable Selection

--- classic_cp/N_Queens/N_Queens.jl
+++ classic_cp/N_Queens/N_Queens.jl
@@ -9,7 +9,7 @@ and  SeaPearl.AllDifferent.
 # Arguments
 - `board_size::Int`: dimension of the board
 """
-function solve_queens(board_size::Int; benchmark=false)::Int
+function solve_queens(board_size::Int; benchmark=false, variableSelection=SeaPearl.MinDomainVariableSelection{false}(), valueSelection=SeaPearl.BasicHeuristic())::Int
     trailer = SeaPearl.Trailer()
     model = SeaPearl.CPModel(trailer)
 
@@ -22,21 +22,18 @@ function solve_queens(board_size::Int; benchmark=false)::Int
     rows_plus = Vector{SeaPearl.AbstractIntVar}(undef, board_size)
     for i = 1:board_size
         rows_plus[i] = SeaPearl.IntVarViewOffset(rows[i], i, rows[i].id*"+"*string(i))
-        #SeaPearl.addVariable!(model, rows_plus[i]; branchable=false)
     end
 
     rows_minus = Vector{SeaPearl.AbstractIntVar}(undef, board_size)
     for i = 1:board_size
         rows_minus[i] = SeaPearl.IntVarViewOffset(rows[i], -i, rows[i].id*"-"*string(i))
-            #SeaPearl.addVariable!(model, rows_minus[i]; branchable=false)
     end
 
     push!(model.constraints, SeaPearl.AllDifferent(rows, trailer))
     push!(model.constraints, SeaPearl.AllDifferent(rows_plus, trailer))
     push!(model.constraints, SeaPearl.AllDifferent(rows_minus, trailer))
 
-    variableSelection = SeaPearl.MinDomainVariableSelection{false}()
-    status = @time SeaPearl.solve!(model; variableHeuristic=variableSelection)
+    status = @time SeaPearl.solve!(model; variableHeuristic=variableSelection, valueSelection=valueSelection)
 
     return length(model.solutions)
 end
