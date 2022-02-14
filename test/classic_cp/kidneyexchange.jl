fd162f2f9e8051c2be465884b618ba9d887fcca1
marco novaes <marcool_98@hotmail.com>
2022-02-14 11:55:16 -0500

    Add tests for KEP

--- test/classic_cp/kidneyexchange.jl
+++ test/classic_cp/kidneyexchange.jl
@@ -1,50 +1,48 @@
 using Test
-using SeaPearl
 include("../../classic_cp/kidneyexchange/kidneyexchange.jl")
 
-# TODO why error with getInputData("data/someInstace")?
-reductibleInstance = """13 0.1
-
-1 4 5
-2
-3 9
-6 8
-7
-
-
-10
-
-13
-11
-12"""
-
-reductibleInstance = parseInput(reductibleInstance)
-
-unreductibleInstance = """8 0.2
-2 3
-1 6
-1 4 7
-2
-2
-5
-8
-3"""
-
-unreductibleInstance = parseInput(unreductibleInstance)
+const pathToData = "../classic_cp/kidneyexchange/data/"
+instancesToSolve = ["kep_13_0.1", "kep_20_0.3", "kep_40_0.05"]
+expectedBestScores = [6, 20, 29]
 
 @testset "kidneyexchange.jl" begin
     @testset "reduce_instance()" begin
-        
         # reductibleInstance
+        reductibleInstance = getInputData(pathToData*"kep_13_0.1")
         canReduce, pairsEquivalence, reduced_c = reduce_instance(reductibleInstance)
-        
+        expectedPairsEquivalence = [2, 3, 4, 11, 12, 13]
+        expectedReduced_c = [[3], [1], [2], [6], [4], [5]]
         @test canReduce
-        @test length(pairsEquivalence) < reductibleInstance.numberOfPairs
-        @test length(pairsEquivalence) == length(reduced_c)
+        @test expectedPairsEquivalence == pairsEquivalence
+        @test expectedReduced_c == reduced_c
 
         # unreductibleInstance
+        unreductibleInstance = getInputData(pathToData*"kep_8_0.2")
         canReduce, pairsEquivalence, reduced_c = reduce_instance(unreductibleInstance)
-
         @test !canReduce
+    end
+
+    @testset "solve_kidneyexchange_matrix()" begin
+        for i in 1:length(instancesToSolve)
+            solved_model = solve_kidneyexchange_matrix(pathToData*instancesToSolve[i])
+            solutions = solved_model.statistics.solutions
+            numberOfPairs = trunc(Int, sqrt(length(solved_model.variables) - 1))
+            realSolutions = filter(e -> !isnothing(e),solutions)
+            bestScores = map(e -> -minimum(values(e)),realSolutions)
+            bestScore = maximum(bestScores)
+            @test bestScore == expectedBestScores[i]
+        end
+    end
+
+    @testset "solve_kidneyexchange_vector()" begin
+        for i in 1:length(instancesToSolve)
+            solved_model = solve_kidneyexchange_vector(pathToData*instancesToSolve[i])
+            solutions = solved_model.statistics.solutions
+            numberOfPairs = trunc(Int, sqrt(length(solved_model.variables) - 1))
+            realSolutions = filter(e -> !isnothing(e),solutions)
+            bestScores = map(e -> -minimum(values(e)),realSolutions)
+            bestScore = maximum(bestScores)
+            @test bestScore == expectedBestScores[i]
+        end
     end
 end
