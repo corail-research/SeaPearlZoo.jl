9474604ed5b7bdce5b27ff7f4226fe2df2ec0a05
marco-novaes98 <43818023+marco-novaes98@users.noreply.github.com>
2022-02-21 14:54:59 -0500

    Minimal fixing to coloring.jl (#18)

--- classic_cp/coloring/coloring.jl
+++ classic_cp/coloring/coloring.jl
@@ -19,7 +19,6 @@ end
 
 include("IOmanager.jl")
 
-
 function outputFromSeaPearl(sol::SeaPearl.Solution; optimality=false)
     numberOfColors = 0
     edgeColors = Int[]
@@ -31,15 +30,11 @@ function outputFromSeaPearl(sol::SeaPearl.Solution; optimality=false)
         end
         push!(edgeColors, color)
     end
-
     return OutputData(numberOfColors, edgeColors, optimality)
 end
 
-
-
 function solve_coloring(input_file; benchmark=false)
     input = getInputData(input_file)
-
     trailer = SeaPearl.Trailer()
     model = SeaPearl.CPModel(trailer)
 
@@ -72,60 +67,63 @@ function solve_coloring(input_file; benchmark=false)
     end
     SeaPearl.addObjective!(model, numberOfColors)
 
-
+    # TODO fix function selectVariable in order to use it for solving the problem
     ### Variable selection heurstic ###
-    function selectVariable(model::SeaPearl.CPModel, sortedPermutation, degrees)
-        maxDegree = 0
-        toReturn = nothing
-        for i in sortedPermutation
-            if !SeaPearl.isbound(model.variables[string(i)])
-                if isnothing(toReturn)
-                    toReturn = model.variables[string(i)]
-                    maxDegree = degrees[i]
-                end
-                if degrees[i] < maxDegree
-                    return toReturn
-                end
+    # function selectVariable(model::SeaPearl.CPModel, sortedPermutation, degrees)
+    #     maxDegree = 0
+    #     toReturn = nothing
+    #     for i in sortedPermutation
+    #         if !SeaPearl.isbound(model.variables[string(i)])
+    #             if isnothing(toReturn)
+    #                 toReturn = model.variables[string(i)]
+    #                 maxDegree = degrees[i]
+    #             end
+    #             if degrees[i] < maxDegree
+    #                 return toReturn
+    #             end
 
-                if length(model.variables[string(i)].domain) < length(toReturn.domain)
-                    toReturn = model.variables[string(i)]
-                end
-            end
-        end
-        return toReturn
-    end
+    #             if length(model.variables[string(i)].domain) < length(toReturn.domain)
+    #                 toReturn = model.variables[string(i)]
+    #             end
+    #         end
+    #     end
+    #     return toReturn
+    # end
 
-    return model
+    # return model
 
+    # TODO: When selectVariable() will work, remove the current call to solve!() to use the commented one
+    SeaPearl.solve!(model; variableHeuristic=SeaPearl.MinDomainVariableSelection{false}(), valueSelection=SeaPearl.BasicHeuristic())
+    # status = SeaPearl.solve!(model; variableHeuristic=((m; cpmodel=nothing) -> selectVariable(m, sortedPermutation, degrees)))
 
-    status = SeaPearl.solve!(model; variableHeuristic=((m; cpmodel=nothing) -> selectVariable(m, sortedPermutation, degrees)))
     if !benchmark
-        for oneSolution in model.statistics.solution
+        for oneSolution in model.statistics.solutions
             output = outputFromSeaPearl(oneSolution)
             printSolution(output)
         end
     end
-    return status
 end
 
-using Gadfly
-using LightGraphs
-using GraphPlot
+# TODO remove those 3 commented lines if packages are not needed
+# using Gadfly
+# using LightGraphs
+# using GraphPlot
 
-function testGraph(input_file)
-    model = solve_coloring(input_file)
-    g = SeaPearl.CPLayerGraph(model)
-    nodelabel = map((x) -> SeaPearl.labelOfVertex(g, x)[1], collect(1:nv(g)))
+# TODO remove function testGraph() if not usefull
+# function testGraph(input_file)
+#     model = solve_coloring(input_file)
+#     g = SeaPearl.CPLayerGraph(model)
+#     nodelabel = map((x) -> SeaPearl.labelOfVertex(g, x)[1], collect(1:nv(g)))
 
-    nlist = Vector{Int}[] # two shells
-    push!(nlist, collect((g.numberOfConstraints+1):(g.numberOfConstraints+g.numberOfVariables))) # second shell
-    push!(nlist, collect((g.numberOfConstraints+g.numberOfVariables+1):(g.numberOfConstraints+g.numberOfVariables+g.numberOfValues))) # second shell
-    push!(nlist, collect(1:g.numberOfConstraints)) # first shell
+#     nlist = Vector{Int}[] # two shells
+#     push!(nlist, collect((g.numberOfConstraints+1):(g.numberOfConstraints+g.numberOfVariables))) # second shell
+#     push!(nlist, collect((g.numberOfConstraints+g.numberOfVariables+1):(g.numberOfConstraints+g.numberOfVariables+g.numberOfValues))) # second shell
+#     push!(nlist, collect(1:g.numberOfConstraints)) # first shell
 
-    membership = map((x) -> SeaPearl.labelOfVertex(g, x)[2], collect(1:nv(g)))
-    nodecolor = [colorant"red", colorant"purple", colorant"green"]
-    # membership color
-    nodefillc = nodecolor[membership]
-    # locs_x, locs_y = shell_layout(g, nlist)
-    return g, nodefillc, nodelabel
-end
+#     membership = map((x) -> SeaPearl.labelOfVertex(g, x)[2], collect(1:nv(g)))
+#     nodecolor = [colorant"red", colorant"purple", colorant"green"]
+#     # membership color
+#     nodefillc = nodecolor[membership]
+#     # locs_x, locs_y = shell_layout(g, nlist)
+#     return g, nodefillc, nodelabel
+# end
