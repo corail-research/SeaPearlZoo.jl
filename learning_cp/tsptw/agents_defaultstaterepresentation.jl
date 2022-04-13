50d021ff32b04dc99dad209b9e1ebb6ced1f405d
louis-gautier <louisgautier99@gmail.com>
2022-04-13 11:32:27 -0400

    Adapted TSPTW to DefaultStateRepresentation

---
+++ learning_cp/tsptw/agents_defaultstaterepresentation.jl
@@ -0,0 +1,61 @@
+agent = RL.Agent(
+    policy = RL.QBasedPolicy(
+        learner = RL.DQNLearner(
+            approximator = RL.NeuralNetworkApproximator(
+                model = SeaPearl.CPNN(
+                    graphChain = Flux.Chain(
+                        SeaPearl.GraphConv(numInFeatures => 32, Flux.leakyrelu),
+                        SeaPearl.GraphConv(32 => 32,  Flux.leakyrelu),
+                        SeaPearl.GraphConv(32 => 32,  Flux.leakyrelu),
+                        SeaPearl.GraphConv(32 => 32,  Flux.leakyrelu),
+                    ),
+                    nodeChain = Flux.Chain(
+                        Flux.Dense(32, 32, relu),
+                        Flux.Dense(32, 32, relu),
+                    ),
+                    outputChain = Flux.Dense(32, 21),
+                ),
+                optimizer = ADAM(0.0001f0)
+            ),
+            target_approximator = RL.NeuralNetworkApproximator(
+                model = SeaPearl.CPNN(
+                    graphChain = Flux.Chain(
+                        SeaPearl.GraphConv(numInFeatures => 32,  Flux.leakyrelu),
+                        SeaPearl.GraphConv(32 => 32,  Flux.leakyrelu),
+                        SeaPearl.GraphConv(32 => 32,  Flux.leakyrelu),
+                        SeaPearl.GraphConv(32 => 32,  Flux.leakyrelu),
+                    ),
+                    nodeChain = Flux.Chain(
+                        Flux.Dense(32, 32, relu),
+                        Flux.Dense(32, 32, relu),
+                    ),
+                    outputChain = Flux.Dense(32, 21),
+                ),
+                optimizer = ADAM(0.0001f0)
+            ),
+            loss_func = Flux.Losses.huber_loss,
+            stack_size = nothing,
+            γ = 0.99f0,
+            batch_size = 1,#32,
+            update_horizon = 1,
+            min_replay_history = 1,
+            update_freq = 1,
+            target_update_freq = 100,
+        ),
+        explorer = RL.EpsilonGreedyExplorer(
+            ϵ_stable = 0.01,
+            kind = :exp,
+            ϵ_init = 1.0,
+            warmup_steps = 0,
+            decay_steps = 2000,
+            step = 1,
+            is_break_tie = false,
+            #is_training = true,
+            rng = MersenneTwister(33)
+        )
+    ),
+    trajectory = RL.CircularArraySARTTrajectory(
+        capacity = 4000,
+        state = SeaPearl.DefaultTrajectoryState[] => ()
+    )
+)
