623c648302ed126df3a372902240f23df336cde7
Tristan François <tpv.francois@gmail.com>
2022-05-20 15:52:52 -0400

    Switch experiment representation of graph coloring to generic

--- learning_cp/comparison/graphcoloring.jl
+++ learning_cp/comparison/graphcoloring.jl
@@ -8,7 +8,7 @@ include("comparison.jl")
 ######### 
 ###############################################################################
 
-function experiment_representation(n_nodes, n_min_color, density, n_episodes, n_instances; n_layers_graph=2, n_eval=10)
+function experiment_representation_graphcoloring(n_nodes, n_min_color, density, n_episodes, n_instances; n_layers_graph=2, n_eval=10, reward=SeaPearl.GeneralReward)
     """
     Compare three agents:
         - an agent with the default representation and default features;
@@ -16,90 +16,6 @@ function experiment_representation(n_nodes, n_min_color, density, n_episodes, n_
         - an agent with the heterogeneous representation and chosen features.
     """
     coloring_generator = SeaPearl.ClusterizedGraphColoringGenerator(n_nodes, n_min_color, density)
-    SR_default = SeaPearl.DefaultStateRepresentation{SeaPearl.DefaultFeaturization,SeaPearl.DefaultTrajectoryState}
-    SR_heterogeneous = SeaPearl.HeterogeneousStateRepresentation{SeaPearl.DefaultFeaturization,SeaPearl.HeterogeneousTrajectoryState}
-
-    agent_default_default = get_default_agent(;
-        capacity=2000,
-        decay_steps=2000,
-        ϵ_stable=0.01,
-        batch_size=16,
-        update_horizon=8,
-        min_replay_history=256,
-        update_freq=1,
-        target_update_freq=8,
-        feature_size=3,
-        conv_size=8,
-        dense_size=16,
-        output_size=n_nodes,
-        n_layers_graph=n_layers_graph,
-        n_layers_node=2,
-        n_layers_output=2
-    )
-    learned_heuristic_default_default = SeaPearl.SimpleLearnedHeuristic{SR_default,SeaPearl.GeneralReward,SeaPearl.FixedOutput}(agent_default_default)
-
-    chosen_features = Dict(
-        "constraint_type" => true,
-        "variable_initial_domain_size" => true,
-        "values_onehot" => true,
-    )
-
-    agent_default_chosen = get_default_agent(;
-        capacity=2000,
-        decay_steps=2000,
-        ϵ_stable=0.01,
-        batch_size=16,
-        update_horizon=8,
-        min_replay_history=256,
-        update_freq=1,
-        target_update_freq=8,
-        feature_size=n_nodes + 6,
-        conv_size=8,
-        dense_size=16,
-        output_size=n_nodes,
-        n_layers_graph=n_layers_graph,
-        n_layers_node=2,
-        n_layers_output=2
-    )
-    learned_heuristic_default_chosen = SeaPearl.SimpleLearnedHeuristic{SR_default,SeaPearl.GeneralReward,SeaPearl.FixedOutput}(agent_default_chosen; chosen_features=chosen_features)
-
-    agent_heterogeneous = get_heterogeneous_agent(;
-        capacity=2000,
-        decay_steps=2000,
-        ϵ_stable=0.01,
-        batch_size=16,
-        update_horizon=8,
-        min_replay_history=256,
-        update_freq=1,
-        target_update_freq=8,
-        feature_size=[1, 2, n_nodes],
-        conv_size=8,
-        dense_size=16,
-        output_size=n_nodes,
-        n_layers_graph=n_layers_graph,
-        n_layers_node=2,
-        n_layers_output=2
-    )
-    learned_heuristic_heterogeneous = SeaPearl.SimpleLearnedHeuristic{SR_heterogeneous,SeaPearl.GeneralReward,SeaPearl.FixedOutput}(agent_heterogeneous; chosen_features=chosen_features)
-
-
-    # Basic value-selection heuristic
-    selectMin(x::SeaPearl.IntVar; cpmodel=nothing) = SeaPearl.minimum(x.domain)
-    heuristic_min = SeaPearl.BasicHeuristic(selectMin)
-
-    learnedHeuristics = OrderedDict(
-        "defaultdefault" => learned_heuristic_default_default,
-        "defaultchosen" => learned_heuristic_default_chosen,
-        "heterogeneous" => learned_heuristic_heterogeneous,
-    )
-    basicHeuristics = OrderedDict(
-        "min" => heuristic_min
-    )
-
-    # -------------------
-    # Variable Heuristic definition
-    # -------------------
-    variableHeuristic = SeaPearl.MinDomainVariableSelection{false}()
 
     expParameters = Dict(
         :generatorParameters => Dict(
@@ -109,32 +25,21 @@ function experiment_representation(n_nodes, n_min_color, density, n_episodes, n_
         ),
     )
 
-    metricsArray, eval_metricsArray = trytrain(
-        nbEpisodes=n_episodes,
-        evalFreq=Int(floor(n_episodes / n_eval)),
-        nbInstances=n_instances,
-        restartPerInstances=1,
-        generator=coloring_generator,
-        variableHeuristic=variableHeuristic,
-        learnedHeuristics=learnedHeuristics,
-        basicHeuristics=basicHeuristics;
-        out_solver=true,
-        verbose=false,
-        expParameters=expParameters,
-        nbRandomHeuristics=0,
-        exp_name="graphcoloring_type1_" * string(n_episodes) * "_" * string(n_nodes) * "_"
+    experiment_representation(n_nodes, n_episodes, n_instances;
+        chosen_features=nothing,
+        feature_sizes = [3, 9, [2, 3, 1]], 
+        output_size = n_nodes, 
+        generator = coloring_generator, 
+        expParameters = expParameters, 
+        basicHeuristics=nothing, 
+        n_layers_graph=n_layers_graph, 
+        n_eval=n_eval, 
+        reward=reward, 
+        type="nqueens", 
     )
 end
 
-# println("start experiment_1")
-# experiment_1(10, 5, 0.95, 2001, 1)
-# println("end experiment_1")
-# println("start experiment_2")
-# experiment_1(15, 5, 0.95, 2001, 1)
-# println("end experiment_2")
-# println("start experiment_3")
-# experiment_1(20, 5, 0.95, 2001, 1)
-# println("end experiment_3")
+experiment_representation_graphcoloring(10, 5, 0.95, 1001, 10; n_layers_graph=3)
 
 ###############################################################################
 ######### Experiment Type 2
@@ -298,4 +203,4 @@ function experiment_chosen_features_heterogeneous_graphcoloring(n_nodes, n_min_c
         )
 end
 
-experiment_chosen_features_heterogeneous_graphcoloring(10, 5, 0.95, 1001, 10)
+# experiment_chosen_features_heterogeneous_graphcoloring(10, 5, 0.95, 1001, 10)
