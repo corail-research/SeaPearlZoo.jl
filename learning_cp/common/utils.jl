19d5d09d9c8209220ed629fded834e9909d95ea4
Tristan François <tpv.francois@gmail.com>
2022-06-01 11:25:31 -0400

    Add hgt in utils

--- learning_cp/common/utils.jl
+++ learning_cp/common/utils.jl
@@ -248,6 +248,153 @@ function get_heterogeneous_graph_chain(original_features_size, mid, out, n_layer
     end
 end
 
+struct HeterogeneousModelHGT1
+    layer1::SeaPearl.HeterogeneousGraphConvInit
+end
+
+function (m::HeterogeneousModelHGT1)(fg)
+    out1 = m.layer1(fg)
+    return out1
+end
+
+Flux.@functor HeterogeneousModelHGT1
+
+struct HeterogeneousModelHGT2
+    layer1::SeaPearl.HeterogeneousGraphConvInit
+    layer2::SeaPearl.HeterogeneousGraphTransformer
+end
+
+function (m::HeterogeneousModelHGT2)(fg)
+    original_fg = fg
+    out1 = m.layer1(fg)
+    out2 = m.layer2(out1)
+    return out2
+end
+
+Flux.@functor HeterogeneousModelHGT2
+
+struct HeterogeneousModelHGT3
+    layer1::SeaPearl.HeterogeneousGraphConvInit
+    layer2::SeaPearl.HeterogeneousGraphTransformer
+    layer3::SeaPearl.HeterogeneousGraphTransformer
+end
+
+function (m::HeterogeneousModelHGT3)(fg)
+    original_fg = fg
+    out1 = m.layer1(fg)
+    out2 = m.layer2(out1)
+    out3 = m.layer3(out2)
+    return out3
+end
+
+Flux.@functor HeterogeneousModelHGT3
+
+struct HeterogeneousModelHGT4
+    layer1::SeaPearl.HeterogeneousGraphConvInit
+    layer2::SeaPearl.HeterogeneousGraphTransformer
+    layer3::SeaPearl.HeterogeneousGraphTransformer
+    layer4::SeaPearl.HeterogeneousGraphTransformer
+end
+
+function (m::HeterogeneousModelHGT4)(fg)
+    original_fg = fg
+    out1 = m.layer1(fg)
+    out2 = m.layer2(out1)
+    out3 = m.layer3(out2)
+    out4 = m.layer4(out3)
+    return out4
+end
+
+Flux.@functor HeterogeneousModelHGT4
+
+struct HeterogeneousModelHGT5
+    layer1::SeaPearl.HeterogeneousGraphConvInit
+    layer2::SeaPearl.HeterogeneousGraphTransformer
+    layer3::SeaPearl.HeterogeneousGraphTransformer
+    layer4::SeaPearl.HeterogeneousGraphTransformer
+    layer5::SeaPearl.HeterogeneousGraphTransformer
+end
+
+function (m::HeterogeneousModelHGT5)(fg)
+    original_fg = fg
+    out1 = m.layer1(fg)
+    out2 = m.layer2(out1)
+    out3 = m.layer3(out2)
+    out4 = m.layer4(out3)
+    out5 = m.layer5(out4)
+    return out5
+end
+
+Flux.@functor HeterogeneousModelHGT5
+
+struct HeterogeneousModelHGT6
+    layer1::SeaPearl.HeterogeneousGraphConvInit
+    layer2::SeaPearl.HeterogeneousGraphTransformer
+    layer3::SeaPearl.HeterogeneousGraphTransformer
+    layer4::SeaPearl.HeterogeneousGraphTransformer
+    layer5::SeaPearl.HeterogeneousGraphTransformer
+    layer6::SeaPearl.HeterogeneousGraphTransformer
+end
+
+function (m::HeterogeneousModelHGT6)(fg)
+    original_fg = fg
+    out1 = m.layer1(fg)
+    out2 = m.layer2(out1)
+    out3 = m.layer3(out2)
+    out4 = m.layer4(out3)
+    out5 = m.layer5(out4)
+    out6 = m.layer6(out5)
+    return out6
+end
+
+Flux.@functor HeterogeneousModelHGT6
+
+get_heterogeneous_graph_conv_layer_hgt(in, heads) = SeaPearl.HeterogeneousGraphTransformer(in, heads)
+
+function get_heterogeneous_graph_chain_hgt(original_features_size, out, n_layers, heads)
+    @assert n_layers >= 1 and n_layers <= 6
+    if n_layers == 1
+        return HeterogeneousModelHGT1(
+            get_heterogeneous_graph_conv_init_layer(original_features_size, out)
+        )
+    elseif n_layers == 2
+        return HeterogeneousModelHGT2(
+            get_heterogeneous_graph_conv_init_layer(original_features_size, out),
+            get_heterogeneous_graph_conv_layer_hgt(out, heads)
+        )
+    elseif n_layers == 3
+        return HeterogeneousModelHGT3(
+            get_heterogeneous_graph_conv_init_layer(original_features_size, out),
+            get_heterogeneous_graph_conv_layer_hgt(out, heads),
+            get_heterogeneous_graph_conv_layer_hgt(out, heads),
+        )
+    elseif n_layers == 4
+        return HeterogeneousModelHGT4(
+            get_heterogeneous_graph_conv_init_layer(original_features_size, out),
+            get_heterogeneous_graph_conv_layer_hgt(out, heads),
+            get_heterogeneous_graph_conv_layer_hgt(out, heads),
+            get_heterogeneous_graph_conv_layer_hgt(out, heads),
+        )
+    elseif n_layers == 5
+        return HeterogeneousModelHGT5(
+            get_heterogeneous_graph_conv_init_layer(original_features_size, out),
+            get_heterogeneous_graph_conv_layer_hgt(out, heads),
+            get_heterogeneous_graph_conv_layer_hgt(out, heads),
+            get_heterogeneous_graph_conv_layer_hgt(out, heads),
+            get_heterogeneous_graph_conv_layer_hgt(out, heads)
+        )
+    elseif n_layers == 6
+        return HeterogeneousModelHGT6(
+            get_heterogeneous_graph_conv_init_layer(original_features_size, out),
+            get_heterogeneous_graph_conv_layer_hgt(out, heads),
+            get_heterogeneous_graph_conv_layer_hgt(out, heads),
+            get_heterogeneous_graph_conv_layer_hgt(out, heads),
+            get_heterogeneous_graph_conv_layer_hgt(out, heads),
+            get_heterogeneous_graph_conv_layer_hgt(out, heads)
+        )
+    end
+end
+
 function get_heterogeneous_cpnn(;feature_size, conv_size, dense_size, output_size, n_layers_graph, n_layers_node, n_layers_output)
     return SeaPearl.HeterogeneousCPNN(
         graphChain=get_heterogeneous_graph_chain(feature_size, conv_size, conv_size, n_layers_graph),
@@ -270,6 +417,14 @@ function get_heterogeneous_variableoutputcpnn(;feature_size, conv_size, dense_si
         get_heterogeneous_graph_chain(feature_size, conv_size, conv_size, n_layers_graph),
         get_dense_chain(conv_size, dense_size, dense_size, n_layers_node),
         get_dense_chain(2*dense_size, dense_size, output_size, n_layers_output)
+    )
+end
+
+function get_heterogeneous_cpnn_hgt(;feature_size, conv_size, dense_size, output_size, n_layers_graph, n_layers_node, n_layers_output, heads)
+    return SeaPearl.HeterogeneousCPNN(
+        graphChain=get_heterogeneous_graph_chain_hgt(feature_size, conv_size, n_layers_graph, heads),
+        nodeChain=get_dense_chain(conv_size, dense_size, dense_size, n_layers_node),
+        outputChain=get_dense_chain(dense_size, dense_size, output_size, n_layers_output)
     )
 end
 
