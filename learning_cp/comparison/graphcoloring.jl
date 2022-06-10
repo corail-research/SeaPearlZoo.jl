16413e255a181ef17723c4011521cd3c765254b3
ziadelassal <ziadelassal@gmail.com>
2022-06-10 14:52:04 -0400

    Add activation functions

--- learning_cp/comparison/graphcoloring.jl
+++ learning_cp/comparison/graphcoloring.jl
@@ -459,3 +459,46 @@ function experiment_restart_heterogeneous_graphcoloring(n_nodes, n_min_color, de
         trajectory_capacity=trajectory_capacity
     )
 end
+
+###############################################################################
+######### Experiment Type 11
+#########  
+######### 
+###############################################################################
+function experiment_activation_heterogeneous_graphcoloring(n_nodes, n_min_color, density, n_episodes, n_instances; n_layers_graph=3, n_eval=10, reward=SeaPearl.GeneralReward, pool = SeaPearl.sumPooling())
+    """
+    Compare four activation functions.
+    """
+    coloring_generator = SeaPearl.ClusterizedGraphColoringGenerator(n_nodes, n_min_color, density)
+    
+    expParameters = Dict(
+        :generatorParameters => Dict(
+            :nbNodes => n_nodes,
+            :nbMinColor => n_min_color,
+            :density => density
+        ),
+        :pooling => string(pool)
+    )
+
+    # Basic value-selection heuristic
+    selectMin(x::SeaPearl.IntVar; cpmodel=nothing) = SeaPearl.minimum(x.domain)
+    heuristic_min = SeaPearl.BasicHeuristic(selectMin)
+    basicHeuristics = OrderedDict(
+        "min" => heuristic_min
+    )
+
+    experiment_activation_heterogeneous(n_nodes, n_episodes, n_instances;
+        chosen_features=nothing,
+        feature_size = [2, 3, 1], 
+        output_size = n_nodes, 
+        generator = coloring_generator, 
+        n_layers_graph = n_layers_graph, 
+        n_eval = n_eval, 
+        reward = reward, 
+        type = "graphcoloring",
+        decay_steps=2000,
+        c=2.0,
+        basicHeuristics=basicHeuristics,
+        pool = pool
+    )
+end
