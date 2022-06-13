33a3e7a423414f2a5678e8dd56044eee7d6dce2b
louis-gautier <louisgautier99@gmail.com>
2022-06-13 12:08:23 -0400

    Added simple experiment on KEP

--- learning_cp/comparison/kep.jl
+++ learning_cp/comparison/kep.jl
@@ -193,3 +193,66 @@ function experiment_chosen_features_heterogeneous_kep(n_nodes, density, n_episod
         output_size=2,
         eval_timeout=eval_timeout)
 end
+
+
+###############################################################################
+######### Simple KEP experiment
+#########  
+######### 
+###############################################################################
+
+function simple_experiment_kep(n_nodes, density, n_episodes, n_instances, chosen_features, feature_size; n_eval=10, eval_timeout=60)
+    """
+    Runs a single experiment on KEP
+    """
+    reward = SeaPearl.GeneralReward
+    generator = SeaPearl.KepGenerator(n_nodes, density)
+    n_step_per_episode = n_nodes
+    SR_heterogeneous = SeaPearl.HeterogeneousStateRepresentation{SeaPearl.DefaultFeaturization,SeaPearl.HeterogeneousTrajectoryState}
+    trajectory_capacity = 500*n_step_per_episode
+    update_horizon = Int(round(n_step_per_episode//2))
+    learnedHeuristics = OrderedDict{String,SeaPearl.LearnedHeuristic}()
+    agent_hetcpnn = get_heterogeneous_agent(;
+            get_heterogeneous_trajectory = () -> get_heterogeneous_slart_trajectory(capacity=trajectory_capacity, n_actions=2),        
+            get_explorer = () -> get_epsilon_greedy_explorer(2000, 0.01),
+            batch_size=16,
+            update_horizon=update_horizon,
+            min_replay_history=Int(round(16*n_step_per_episode//2)),
+            update_freq=1,
+            target_update_freq=7*n_step_per_episode,
+            get_heterogeneous_nn = () -> get_heterogeneous_fullfeaturedcpnn(
+                feature_size=feature_size,
+                conv_size=8,
+                dense_size=16,
+                output_size=1,
+                n_layers_graph=3,
+                n_layers_node=2,
+                n_layers_output=2
+            )
+        )
+    learned_heuristic_hetffcpnn = SeaPearl.SimpleLearnedHeuristic{SR_heterogeneous,reward,SeaPearl.FixedOutput}(agent_hetcpnn; chosen_features=chosen_features)
+    learnedHeuristics["hetffcpnn"] = learned_heuristic_hetffcpnn
+    variableHeuristic = SeaPearl.MinDomainVariableSelection{false}()
+
+    basicHeuristics = OrderedDict(
+        "random" => SeaPearl.RandomHeuristic()
+    )
+
+    metricsArray, eval_metricsArray = trytrain(
+        nbEpisodes=n_episodes,
+        evalFreq=Int(floor(n_episodes / n_eval)),
+        nbInstances=n_instances,
+        restartPerInstances=1,
+        generator=generator,
+        variableHeuristic=variableHeuristic,
+        learnedHeuristics=learnedHeuristics,
+        basicHeuristics=basicHeuristics;
+        out_solver=true,
+        verbose=true,
+        nbRandomHeuristics=0,
+        exp_name= "heterogeneous_ffcpnn_" * string(n_episodes) * "_" * string(size) * "_",
+        eval_timeout=eval_timeout
+    )
+    nothing
+
+end
