029e6cd7c3d1cb42654df061f744df72e4c34a7b
3rdCore <tom.marty@polytec>
2022-08-23 19:03:13 -0400

    automated plotting and benchmark for any training experiment

--- learning_cp/common/experiment.jl
+++ learning_cp/common/experiment.jl
@@ -11,6 +11,19 @@ using Random
 using LightGraphs
 using OrderedCollections
 using CircularArrayBuffers
+using PyCall
+using Pkg
+using TensorBoardLogger, Logging
+
+"""
+ENV["PYTHON"] = "/home/x86_64-unknown-linux_ol7-gnu/anaconda-2022.05/bin/python"
+using Pkg
+Pkg.build("PyCall")
+run(`$(PyCall.python) -m pip install matplotlib`)
+run(`$(PyCall.python) -m pip install pandas`)
+run(`$(PyCall.python) -m pip install seaborn`)
+run(`$(PyCall.python) -m pip install ipython`)
+"""
 
 # -------------------
 # -------------------
@@ -18,10 +31,14 @@ using CircularArrayBuffers
 # -------------------
 # -------------------
 
-function trytrain(; nbEpisodes::Int, evalFreq::Int, nbInstances::Int, restartPerInstances::Int=1, generator::SeaPearl.AbstractModelGenerator, variableHeuristic::SeaPearl.AbstractVariableSelection=SeaPearl.MinDomainVariableSelection{false}(), learnedHeuristics::OrderedDict{String,<:SeaPearl.LearnedHeuristic}, basicHeuristics::OrderedDict{String,SeaPearl.BasicHeuristic}, base_name="experiment"::String, exp_name=""::String, out_solver=true::Bool, verbose=false::Bool, nbRandomHeuristics=0::Int, eval_timeout=nothing::Union{Nothing, Int}, eval_strategy=SeaPearl.DFSearch(), strategy = SeaPearl.DFSearch(), seedTraining = nothing::Union{Nothing, Int}, seedEval =  nothing, eval_generator=nothing)
+function trytrain(; nbEpisodes::Int, evalFreq::Int, nbInstances::Int, restartPerInstances::Int=1, generator::SeaPearl.AbstractModelGenerator, variableHeuristic::SeaPearl.AbstractVariableSelection=SeaPearl.MinDomainVariableSelection{false}(), learnedHeuristics::OrderedDict{String,<:SeaPearl.LearnedHeuristic}, basicHeuristics::OrderedDict{String,SeaPearl.BasicHeuristic}, base_name="experiment"::String, exp_name=""::String, out_solver=true::Bool, verbose=false::Bool, nbRandomHeuristics=0::Int, eval_timeout=nothing::Union{Nothing, Int},  training_timeout=nothing::Union{Nothing, Int}, eval_every =nothing::Union{Nothing, Int}, eval_strategy=SeaPearl.DFSearch(), strategy = SeaPearl.DFSearch(), seedTraining = nothing::Union{Nothing, Int}, seedEval =  nothing, eval_generator=nothing, logger = nothing)
+
+
     experienceTime = Base.replace("$(round(now(), Dates.Second(3)))", ":" => "-")
     date = split(experienceTime, "T")[1]
     time = split(experienceTime, "T")[2]
+    logger =TBLogger("tensorboard_logs/"*exp_name*date*time, min_level=Logging.Info)
+
     if !isdir(date)
         mkdir(date)
     end
@@ -56,16 +73,19 @@ function trytrain(; nbEpisodes::Int, evalFreq::Int, nbInstances::Int, restartPer
         out_solver=out_solver,
         verbose=verbose,
         evaluator = evaluator,
+        training_timeout = training_timeout,
         restartPerInstances=restartPerInstances,
-        rngTraining = MersenneTwister(seedTraining)
+        rngTraining = MersenneTwister(seedTraining), 
+        eval_every = eval_every, 
+        logger = logger,
     )
 
     #saving model weights
     for (key, lh) in learnedHeuristics
         if (hasfield(typeof(lh.agent.policy),:approximator)) #PPO
-            model = lh.agent.policy.approximator
+            model = Flux.cpu(lh.agent.policy.approximator)
         else #DQN
-            model = lh.agent.policy.learner.approximator
+            model = Flux.cpu(lh.agent.policy.learner.approximator)
         end
         @save dir * "/model_" * key * ".bson" model
     end
@@ -89,5 +109,40 @@ function trytrain(; nbEpisodes::Int, evalFreq::Int, nbInstances::Int, restartPer
         SeaPearlExtras.storedata(eval_metricsArray[:, counter+i]; filename=dir * "/" * base_name * "_random$(i)")
     end
 
+    py"""
+    import sys
+    sys.path.insert(0, "/home/martom/SeaPearl/SeaPearlExtras.jl/src/metrics/basicmetrics/")
+    from benchmarkPy import *
+    import plots
+    import numpy as np
+    def benchmark(path):
+        print_all(path +"/")
+
+    def plot(path,eval):
+        plots.all(path +"/", window=100, estimator=np.mean, ilds= eval)
+    """
+
+    py"plot"(dir, eval_strategy != SeaPearl.DFSearch())
+
+    chosen_features = valueSelectionArray[1].chosen_features
+    feature_size = [6, 5, 2]
+
+    n = 10 # Number of instances to evaluate on
+    budget = 1000 # Budget of visited nodes
+    has_objective = false # Set it to true if we have to branch on the objective variable
+    include_dfs = true # Set it to true if you want to evaluate with DFS in addition to ILDS
+    
+    selectMin(x::SeaPearl.IntVar; cpmodel=nothing) = SeaPearl.minimum(x.domain)
+    heuristic_min = SeaPearl.BasicHeuristic(selectMin)
+    basicHeuristics = OrderedDict(
+        #"Min_heuristic" => heuristic_min,
+    )
+    include("../common/benchmark.jl")
+    Base.invokelatest(benchmark, dir, n, chosen_features, has_objective, generator, basicHeuristics, include_dfs, budget)
+    
+
+    py"benchmark"(dir)
+
     return metricsArray, eval_metricsArray
+
 end
