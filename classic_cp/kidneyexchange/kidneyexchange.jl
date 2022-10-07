bcea705ed8bea708e29c866e9fc03218bc17419f
Léo Boisvert <leo.boisvert@hotmail.ca>
2022-10-07 15:55:13 -0400

    changed variable name to simplify code reading in instance reduction code

--- classic_cp/kidneyexchange/kidneyexchange.jl
+++ classic_cp/kidneyexchange/kidneyexchange.jl
@@ -148,19 +148,19 @@ function solve_kidneyexchange_matrix(inputData::InputData)
         model.adhocInfo = pairsEquivalence
         compatibilities = reduced_compatibilities
     end
-    num_compatibilities = length(compatibilities)
+    num_pairs = length(compatibilities)
     
     # =========VARIABLES=========
     #x[i, j] = 1 => pair i receive a kidney from pair j
     #x[i, j] = 0 => pair i do not receive a kidney from pair j
-    x = Matrix{SeaPearl.AbstractIntVar}(undef, num_compatibilities, num_compatibilities)
+    x = Matrix{SeaPearl.AbstractIntVar}(undef, num_pairs, num_pairs)
 
     #minus_x = x * -1
     #Usefull to check with SumToZero that for each pair: give a kidney <=> receive a kidney
-    minus_x = Matrix{SeaPearl.AbstractIntVar}(undef, num_compatibilities, num_compatibilities) 
+    minus_x = Matrix{SeaPearl.AbstractIntVar}(undef, num_pairs, num_pairs) 
 
-    for i = 1:num_compatibilities
-        for j = 1:num_compatibilities
+    for i = 1:num_pairs
+        for j = 1:num_pairs
             if j ∈ compatibilities[i]
                 x[i, j] = SeaPearl.IntVar(0, 1, "x_"*string(i)*"_"*string(j), model.trailer)
                 SeaPearl.addVariable!(model, x[i, j]; branchable=true)
@@ -175,18 +175,18 @@ function solve_kidneyexchange_matrix(inputData::InputData)
     end
 
     # =========CONSTRAINTS=========
-    for i = 1:num_compatibilities
+    for i = 1:num_pairs
         SeaPearl.addConstraint!(model, SeaPearl.SumLessThan(x[i, :], 1, trailer)) #Check that any pair receives more than 1 kidney
         SeaPearl.addConstraint!(model, SeaPearl.SumLessThan(x[:, i], 1, trailer)) #Check that any pair gives more than 1 kidney
         SeaPearl.addConstraint!(model, SeaPearl.SumToZero(hcat(x[:, i], minus_x[i, :]), trailer)) #Check that for each pair: give a kidney <=> receive a kidney
     end
 
     # =========OBJECTIVE=========
-    minusNumberOfExchanges = SeaPearl.IntVar(-num_compatibilities, 0, "minusNumberOfExchanges", trailer) # SeaPearl minimizes the objective, so we min the negative objective 
+    minusNumberOfExchanges = SeaPearl.IntVar(-num_pairs, 0, "minusNumberOfExchanges", trailer) # SeaPearl minimizes the objective, so we min the negative objective 
     SeaPearl.addVariable!(model, minusNumberOfExchanges; branchable=false)
     vars = SeaPearl.AbstractIntVar[]
 
-    for i in 1:num_compatibilities
+    for i in 1:num_pairs
         vars = cat(vars, x[i, :]; dims=1)
     end
     push!(vars, minusNumberOfExchanges)
@@ -214,45 +214,37 @@ Create a vector (pairsEquivalence) to record the equivalence between the origina
 - c: reduced version of the vector compatibilities
 """
 function reduce_instance(inputData)
-    n = inputData.numberOfPairs
-    c = inputData.compatibilities
+    num_pairs = inputData.numberOfPairs
+    compatibilities = inputData.compatibilities
     canReduce = true
-    current_n = n
+    current_num_pairs = num_pairs
 
     #Remove recursively from vectors in compatibilities the pairs that can't receive nor give any kidney
     while canReduce
-        flatten_c = reduce(vcat,c)
-        usefullPairs = [i for i in 1:n if i in flatten_c && c[i] != []] 
-        if length(usefullPairs) < current_n
-            c = [filter(e -> e in usefullPairs, list) for list in c]
-            current_n = length(usefullPairs)
+        flattened_compatibilities = reduce(vcat,compatibilities)
+        usefulPairs = [i for i in 1:num_pairs if i in flattened_compatibilities && compatibilities[i] != []] 
+        if length(usefulPairs) < current_num_pairs
+            compatibilities = [filter(e -> e in usefulPairs, list) for list in compatibilities]
+            current_num_pairs = length(usefulPairs)
         else
             canReduce = false
         end
     end
 
-    #Check if problem is reduced
-    if n != current_n
-
-        #Create a vector (pairsEquivalence) to record the equivalence between the original instance and the reduced version
-        pairsEquivalence = []
-        for i in 1:n
-            if !isempty(c[i])
+    if num_pairs != current_num_pairs # problem has been reduced
+        pairsEquivalence = [] # Equivalence between the original instance and the reduced version
+        for i in 1:num_pairs
+            if !isempty(compatibilities[i])
                 push!(pairsEquivalence, i)
             end
         end
-    
-        #Remove from compatibilities the isolated pairs
-        c = filter(e -> !isempty(e), c)
-
-        #Update compatibilities using pairsEquivalence
-        for i in 1:length(c)
-            c[i] = map(e -> findfirst(x-> x == e, pairsEquivalence), c[i])
-        end
-
-        println("Instance reduction: original size "*string(n)*", reduced size "*string(current_n))
-        return true, pairsEquivalence, c
+        compatibilities = filter(e -> !isempty(e), compatibilities) #Remove from compatibilities the isolated pairs
         
+        for i in 1: length(compatibilities) #Update compatibilities using pairsEquivalence
+            compatibilities[i] = map(e -> findfirst(x-> x == e, pairsEquivalence), compatibilities[i])
+        end
+        println("Instance reduction: original size "*string(n)*", reduced size "*string(current_num_pairs))
+        return true, pairsEquivalence, compatibilities
     else
         println("Irreducible instance")
         return false, nothing, nothing
