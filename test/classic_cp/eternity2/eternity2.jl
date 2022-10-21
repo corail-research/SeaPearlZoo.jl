57b3cc970725a9ea276d7da2ddc648a8624010d6
3rdCore <tom.marty@polytechnique.edu>
2022-10-21 17:59:41 -0400

    fixed SeaPearlZoo testset pipeline

--- test/classic_cp/eternity2/eternity2.jl
+++ test/classic_cp/eternity2/eternity2.jl
@@ -1,3 +1,3 @@
 @testset "eternity2.jl" begin
-    Main.SeaPearlZoo.solve_eternity2("classic_cp/eternity2/eternity3x3")
+    SeaPearlZoo.solve_eternity2("test/classic_cp/eternity2/eternity3x3")
 end
