d4f95a8a72c855e32807f411512e57cdc71f5934
Tom Marty <59280588+3rdCore@users.noreply.github.com>
2022-10-24 11:47:37 -0400

    quick fix for SeaPearlZoo testset pipeline (#50)
    * fixed SeaPearlZoo testset pipeline
    
    * updated path

--- test/classic_cp/coloring/coloring.jl
+++ test/classic_cp/coloring/coloring.jl
@@ -1,3 +1,3 @@
 @testset "coloring.jl" begin
-    Main.SeaPearlZoo.solve_coloring("classic_cp/coloring/gc_4_1")
+    SeaPearlZoo.solve_coloring("classic_cp/coloring/gc_4_1")
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,10 @@
 using SeaPearl
-include("../src/SeaPearlZoo.jl")
+using SeaPearlZoo
 using Test
 
 @testset "SeaPearlZoo" begin
     include("classic_cp/classic_cp.jl")
 end
 
+revise_user = "You use Revise, you're efficient in your work, well done ;)" 
 revise_user = "You use Revise, you're efficient in your work, well done ;)"
