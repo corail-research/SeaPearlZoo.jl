162f6d491fa4870c39e9037a0fa0b5eb0ffe91a4
Léo Boisvert <leo.boisvert@hotmail.ca>
2022-11-18 16:09:24 -0500

    Fix learning knapsack test

---
+++ test/learning_cp/knapsack/learning_knapsack.jl
@@ -0,0 +1,67 @@
+@testset "learning_coloring.jl" begin
+    
+    knapsack_generator = SeaPearl.KnapsackGenerator(10, 10, 0.2)
+    StateRepresentation = SeaPearl.DefaultStateRepresentation{KnapsackFeaturization, SeaPearl.DefaultTrajectoryState}
+    numInFeatures = SeaPearl.feature_length(StateRepresentation)
+    experiment_setup = SeaPearlZoo.KnapsackExperimentConfig(1, 2, 1, 1)
+    approximator_config = SeaPearlZoo.KnapsackApproximatorConfig(16, 1, SeaPearl.GraphConv(16 => 16, Flux.leakyrelu), false)
+    target_approximator_config = SeaPearlZoo.KnapsackApproximatorConfig(16, 1, SeaPearl.GraphConv(16 => 16, Flux.leakyrelu), false)
+    approximator_model = SeaPearlZoo.build_knapsack_approximator_model(approximator_config)
+    target_approximator_model = SeaPearlZoo.build_knapsack_target_approximator_model(target_approximator_config)
+    knapsack_agent_config = SeaPearlZoo.KnapsackAgentConfig( 0.9f0, 8, 10, 8, 8, 1, experiment_setup.num_episodes)
+    agent = SeaPearlZoo.build_knapsack_agent(approximator_model, target_approximator_model, knapsack_agent_config)
+    
+    # Value Heuristic definition
+    learnedHeuristic = SeaPearl.SimpleLearnedHeuristic{StateRepresentation, knapsackReward, SeaPearl.FixedOutput}(agent)
+    basicHeuristic = SeaPearl.BasicHeuristic((x; cpmodel=nothing) -> SeaPearl.maximum(x.domain)) 
+    
+    # Variable Heuristic definition
+    struct KnapsackVariableSelection <: SeaPearl.AbstractVariableSelection{false} end
+    
+    function (::KnapsackVariableSelection)(model::SeaPearl.CPModel)
+        i = 1
+        while SeaPearl.isbound(model.variables["x[" * string(i) * "]"])
+            i += 1
+        end
+        return model.variables["x[" * string(i) * "]"]
+    end
+    
+    valueSelectionArray = [learnedHeuristic, basicHeuristic]
+    
+    function solve_knapsack_with_learning!(experiment_setup::SeaPearlZoo.KnapsackExperimentConfig, save_experiment_artefacts::Bool=false)
+        experiment_parameters = Dict(
+            :nbEpisodes => experiment_setup.num_episodes,
+            :evalFreq => experiment_setup.eval_freq,
+            :nbInstances => experiment_setup.num_instances
+        )
+        metricsArray, eval_metricsArray = SeaPearl.train!(;
+            valueSelectionArray= valueSelectionArray,
+            generator=knapsack_generator,
+            nbEpisodes=experiment_setup.num_episodes,
+            strategy=SeaPearl.DFSearch(),
+            variableHeuristic=KnapsackVariableSelection(),
+            out_solver=false,
+            verbose=false,
+            evaluator=SeaPearl.SameInstancesEvaluator(
+                valueSelectionArray, 
+                knapsack_generator; 
+                evalFreq=experiment_setup.eval_freq, 
+                nbInstances=experiment_setup.num_instances
+            ),
+            restartPerInstances = 1
+        )
+        if save_experiment_artefacts
+            experience_time = now()
+            dir = mkdir(string("exp_",Base.replace("$(round(experience_time, Dates.Second(3)))",":"=>"-")))
+            open(dir*"/params.json", "w") do file
+                JSON.print(file, experiment_parameters)
+            end    
+            trained_weights = params(agent.policy.learner.approximator.model)
+            @save dir*"/model_weights_knapsack.bson" trained_weights
+        end
+
+        return metricsArray, eval_metricsArray
+    end
+    
+    metricsArray, eval_metricsArray = SeaPearlZoo.solve_knapsack_with_learning!(experiment_setup)
+end
