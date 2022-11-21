7528703d5918e80eb1d8b3ea12ad16754d157869
Léo Boisvert <leo.boisvert@hotmail.ca>
2022-11-21 11:17:36 -0500

    Initial cleanup

--- src/learning_cp/eternity2/eternity2.jl
+++ src/learning_cp/eternity2/eternity2.jl
@@ -10,24 +10,22 @@ using SeaPearlExtras
 include("rewards.jl")
 include("features.jl")
 
-# -------------------
-# Generator
-# -------------------
+
 eternity2_generator = SeaPearl.Eternity2Generator(6,6,6)
+
 # -------------------
 # Internal variables
 # -------------------
-
 SR = SeaPearl.DefaultStateRepresentation{EternityFeaturization, SeaPearl.DefaultTrajectoryState}
-numInFeatures = SeaPearl.feature_length(SR)
-numGlobalFeature = SeaPearl.global_feature_length(SR)
+num_input_features = SeaPearl.feature_length(SR)
+num_global_features = SeaPearl.global_feature_length(SR)
 # -------------------
 # Experience variables
 # -------------------
-nbEpisodes = 100
-evalFreq = 30
-nbInstances = 1
-nbRandomHeuristics = 1
+num_episodes = 100
+eval_freq = 30
+num_instances = 1
+num_random_heuristics = 1
 
 # -------------------
 # Agent definition
@@ -35,13 +33,10 @@ nbRandomHeuristics = 1
 
 include("agents.jl")
 
-# -------------------
-# Value Heuristic definition
-# -------------------
-learnedHeuristic=SeaPearl.LearnedHeuristic{SR, SeaPearl.CPReward, SeaPearl.FixedOutput}(agent)
-# Basic value-selection heuristic
+
+learned_heuristic = SeaPearl.LearnedHeuristic{SR, SeaPearl.CPReward, SeaPearl.FixedOutput}(agent)
 selectMin(x::SeaPearl.IntVar; cpmodel=nothing) = SeaPearl.minimum(x.domain)
-heuristic_min = SeaPearl.BasicHeuristic(selectMin)
+min_domain_heuristic = SeaPearl.BasicHeuristic(selectMin)
 
 function select_random_value(x::SeaPearl.IntVar; cpmodel=nothing)
     selected_number = rand(1:length(x.domain))
@@ -55,40 +50,37 @@ function select_random_value(x::SeaPearl.IntVar; cpmodel=nothing)
     @assert false "This should not happen"
 end
 
-randomHeuristics = []
-for i in 1:nbRandomHeuristics
-    push!(randomHeuristics, SeaPearl.BasicHeuristic(select_random_value))
+random_heuristics = []
+for i in 1:num_random_heuristics
+    push!(random_heuristics, SeaPearl.BasicHeuristic(select_random_value))
 end
 
-valueSelectionArray = [learnedHeuristic, heuristic_min]
-append!(valueSelectionArray, randomHeuristics)
-# -------------------
-# Variable Heuristic definition
-# -------------------
-variableSelection = SeaPearl.MinDomainVariableSelection{false}()
-
-# -------------------
-# -------------------
-# Core function
-# -------------------
-# -------------------
-
-function trytrain(nbEpisodes::Int)
+value_selection_array = [learned_heuristic, min_domain_heuristic]
+append!(value_selection_array, random_heuristics)
+variable_selection = SeaPearl.MinDomainVariableSelection{false}()
 
 
-    metricsArray, eval_metricsArray = SeaPearl.train!(
-        valueSelectionArray=valueSelectionArray,
+function trytrain(num_episodes::Int)
+
+    instance_evaluator = SeaPearl.SameInstancesEvaluator(
+        value_selection_array,
+        eternity2_generator; 
+        evalFreq=eval_freq, 
+        nbInstances=num_instances
+    )
+    metrics_array, eval_metrics_array = SeaPearl.train!(
+        valueSelectionArray=value_selection_array,
         generator=eternity2_generator,
-        nbEpisodes=nbEpisodes,
+        nbEpisodes=num_episodes,
         strategy=SeaPearl.DFSearch(),
-        variableHeuristic=variableSelection,
+        variableHeuristic=variable_selection,
         out_solver=false,
         verbose = true,
-        evaluator=SeaPearl.SameInstancesEvaluator(valueSelectionArray,eternity2_generator; evalFreq = evalFreq, nbInstances = nbInstances),
+        evaluator=instance_evaluator,
         restartPerInstances = 1
     )
 
-    return metricsArray, eval_metricsArray
+    return metrics_array, eval_metrics_array
 end
 
 
@@ -96,5 +88,4 @@ end
 # -------------------
 # -------------------
 
-metricsArray, eval_metricsArray = trytrain(nbEpisodes)
-nothing
+metrics_array, eval_metrics_array = trytrain(num_episodes)
