8d8f7dfc3647d9beb9303c309faa5ad555ddacf0
Léo Boisvert <leo.boisvert@hotmail.ca>
2022-11-25 14:23:40 -0500

    Main file refactor

--- src/learning_cp/latin/latin.jl
+++ src/learning_cp/latin/latin.jl
@@ -1,124 +1,180 @@
-using SeaPearl
-using SeaPearlExtras
+using BSON: @load, @save
+using Dates
+using Flux
+# using GeometricFlux
+using JSON
+using LightGraphs
+using Random
 using ReinforcementLearning
 const RL = ReinforcementLearning
-using Flux
-using GeometricFlux
-using JSON
-using BSON: @load, @save
-using Random
-using Dates
-using LightGraphs
+using SeaPearl
 
-include("rewards.jl")
-include("features.jl")
-
-# -------------------
-# Generator
-# -------------------
-N = 11
-p = 0.6
-latin_generator = SeaPearl.LatinGenerator(N,p)
-# -------------------
-# Internal variables
-# -------------------
-
-SR = SeaPearl.DefaultStateRepresentation{LatinFeaturization, SeaPearl.DefaultTrajectoryState}
-numInFeatures = SeaPearl.feature_length(SR)
-numGlobalFeature = SeaPearl.global_feature_length(SR)
-# -------------------
-# Experience variables
-# -------------------
-nbEpisodes = 10
-evalFreq = 30
-nbInstances = 1
-nbRandomHeuristics = 0
-
-# -------------------
-# Agent definition
-# -------------------
-
+include("agent_config.jl")
 include("agents.jl")
+include("featurization.jl")
+include("latin_config.jl")
+include("rewards.jl")
+include("utils.jl")
 
-# -------------------
-# Value Heuristic definition
-# -------------------
-learnedHeuristic=SeaPearl.LearnedHeuristic{SR, InspectReward, SeaPearl.FixedOutput}(agent)
-# Basic value-selection heuristic
+state_representation= SeaPearl.DefaultStateRepresentation{LatinFeaturization, SeaPearl.DefaultTrajectoryState}
+latin_exp_config = LatinExperimentConfig(
+    state_representation,
+    10,
+    30,
+    1,
+    1,
+    0,
+    11,
+    0.6
+)
+
+function SeaPearl.featurize(sr::SeaPearl.DefaultStateRepresentation{LatinFeaturization,TS}) where TS
+    g = sr.cplayergraph
+    features = zeros(Float32, 7, nv(g))
+    for i in 1:nv(g)
+        cp_vertex = SeaPearl.cpVertexFromIndex(g, i)
+        if isa(cp_vertex, SeaPearl.VariableVertex)
+            features[1,i]=1
+            id = cp_vertex.variable.id
+            t = split(id,['_'])[2]
+            a,b = parse(Int, split(t,",")[1]), parse(Int, split(t,",")[2]) #coordinates on the board
+            features[2,i],features[3,i] = a,b
+        elseif isa(cp_vertex, SeaPearl.ConstraintVertex)
+            if isa(cp_vertex.constraint, SeaPearl.EqualConstant)
+                features[5,i] = cp_vertex.constraint.v
+            else
+                features[6,i] = 1
+            end
+        elseif isa(cp_vertex, SeaPearl.ValueVertex)
+            features[7,i] = cp_vertex.value
+        end
+    end
+    features
+end
+
+function SeaPearl.global_featurize(sr::SeaPearl.DefaultStateRepresentation{LatinFeaturization,TS}) where TS
+    N = latin_exp_config.generator.N
+    g = sr.cplayergraph
+    feature = zeros(Float32, N^2)
+    for i in 1:nv(g)
+        cp_vertex = SeaPearl.cpVertexFromIndex(g, i)
+        if isa(cp_vertex, SeaPearl.ConstraintVertex)&&isa(cp_vertex.constraint, SeaPearl.EqualConstant)
+            x = cp_vertex.constraint.x
+            id=x.id
+            v = cp_vertex.constraint.v
+            t = split(id,['_'])[2]
+            a,b = parse(Int, split(t,",")[1]), parse(Int, split(t,",")[2])
+            feature[(a-1)*N + b] = v
+        end
+    end
+    feature
+end
+
+function SeaPearl.update_features!(sr::SeaPearl.DefaultStateRepresentation{LatinFeaturization,TS}, model::SeaPearl.CPModel) where TS
+    N = latin_exp_config.generator.N
+    g = sr.cplayergraph
+    globalFeature = sr.globalFeatures
+    features = sr.nodeFeatures
+    for i in 1:nv(g)
+        cp_vertex = SeaPearl.cpVertexFromIndex(g, i)
+        if isa(cp_vertex, SeaPearl.VariableVertex)
+            var = cp_vertex.variable
+            id = cp_vertex.variable.id
+            t = split(id,['_'])[2]
+            a,b = parse(Int, split(t,",")[1]), parse(Int, split(t,",")[2])
+            if SeaPearl.isbound(var)
+                features[4, i] = SeaPearl.assignedValue(var)
+                globalFeature[(a-1)*N + b] = SeaPearl.assignedValue(var)
+            else
+                features[4, i] = 0
+                globalFeature[(a-1)*N + b] = 0
+            end
+        end
+    end
+end
+
+function SeaPearl.feature_length(::Type{SeaPearl.DefaultStateRepresentation{LatinFeaturization, TS}}) where TS
+    return 7
+end
+
+function SeaPearl.global_feature_length(::Type{SeaPearl.DefaultStateRepresentation{LatinFeaturization, TS}}) where TS
+    return latin_exp_config.generator.N * latin_exp_config.generator.N
+end
+
+model_config = LatinModelConfig(
+    SeaPearl.GraphConv(32 => 32, Flux.leakyrelu),
+    2,
+    SeaPearl.feature_length(state_representation),
+    SeaPearl.global_feature_length(state_representation),
+    latin_exp_config.generator.N,
+    latin_exp_config.generator.p,
+    false,
+)
+approximator_model = build_model(model_config)
+target_approximator_model = build_model(model_config)
+agent_config = LatinAgentConfig(
+    latin_exp_config.generator.N,
+    approximator_model,
+    target_approximator_model,
+    16,
+    3,
+    10,
+    8,
+    100,
+    0.1,
+    2000,
+    1,
+    1000
+)
+agent = build_agent(agent_config)
+
+learned_heuristic = SeaPearl.SimpleLearnedHeuristic{state_representation, InspectReward, SeaPearl.FixedOutput}(agent)
 selectMin(x::SeaPearl.IntVar; cpmodel=nothing) = SeaPearl.minimum(x.domain)
 heuristic_min = SeaPearl.BasicHeuristic(selectMin)
+random_heuristics = []
 
-function select_random_value(x::SeaPearl.IntVar; cpmodel=nothing)
-    selected_number = rand(1:length(x.domain))
-    i = 1
-    for value in x.domain
-        if i == selected_number
-            return value
-        end
-        i += 1
-    end
-    @assert false "This should not happen"
+for i in 1 : latin_exp_config.num_random_heuristics
+    push!(random_heuristics, SeaPearl.BasicHeuristic(select_random_value))
 end
 
-randomHeuristics = []
-for i in 1:nbRandomHeuristics
-    push!(randomHeuristics, SeaPearl.BasicHeuristic(select_random_value))
-end
+value_selection_array = [learned_heuristic, heuristic_min]
+append!(value_selection_array, random_heuristics)
+variable_selection = SeaPearl.MinDomainVariableSelection{false}()
 
-valueSelectionArray = [learnedHeuristic,heuristic_min]
-append!(valueSelectionArray, randomHeuristics)
-# -------------------
-# Variable Heuristic definition
-# -------------------
-variableSelection = SeaPearl.MinDomainVariableSelection{false}()
-
-# -------------------
-# -------------------
-# Core function
-# -------------------
-# -------------------
-
-function trytrain(nbEpisodes::Int)
-    experienceTime = now()
-    dir = mkdir(string("exp_",Base.replace("$(round(experienceTime, Dates.Second(3)))",":"=>"-")))
-    expParameters = Dict(
-        :nbEpisodes => nbEpisodes,
-        :evalFreq => evalFreq,
-        :nbInstances => nbInstances
+function train_latin_agent(
+        agent::RL.Agent,
+        latin_exp_config::LatinExperimentConfig, 
+        value_selection_array::Array,
+        variable_selection::SeaPearl.AbstractVariableSelection,
+        save_experiment_artefacts::Bool=false
     )
-    open(dir*"/params.json", "w") do file
-        JSON.print(file, expParameters)
+    if save_experiment_artefacts
+        directory = save_experiment_config(latin_exp_config)
     end
 
-    metricsArray, eval_metricsArray = SeaPearl.train!(
-        valueSelectionArray=valueSelectionArray,
-        generator=latin_generator,
-        nbEpisodes=nbEpisodes,
-        strategy=SeaPearl.DFSearch(),
-        variableHeuristic=variableSelection,
-        out_solver=false,
-        verbose=false,
-        evaluator=SeaPearl.SameInstancesEvaluator(valueSelectionArray,latin_generator; evalFreq=evalFreq, nbInstances=nbInstances),
-        restartPerInstances=1
+    metrics_array, eval_metrics_array = SeaPearl.train!(
+        valueSelectionArray = value_selection_array,
+        generator = latin_exp_config.generator,
+        nbEpisodes = latin_exp_config.num_episodes,
+        strategy = SeaPearl.DFSearch(),
+        variableHeuristic = variable_selection,
+        out_solver = false,
+        verbose = false,
+        evaluator = SeaPearl.SameInstancesEvaluator(
+            value_selection_array,
+            latin_exp_config.generator; 
+            evalFreq = latin_exp_config.eval_freq, 
+            nbInstances = latin_exp_config.num_instances
+        ),
+        restartPerInstances = latin_exp_config.num_restarts_per_instance
     )
 
-    trained_weights = params(agent.policy.learner.approximator.model)
-    @save dir*"/model_weights_latin"*string(N)*"_"*string(p)*".bson" trained_weights
-
-    SeaPearlExtras.storedata(metricsArray[1]; filename=dir*"/latin_$(N)_$(p)training")
-    SeaPearlExtras.storedata(eval_metricsArray[:,1]; filename=dir*"/latin_$(N)_$(p)_trained")
-    SeaPearlExtras.storedata(eval_metricsArray[:,2]; filename=dir*"/latin_$(N)_$(p)_min")
-    for i = 1:nbRandomHeuristics
-        SeaPearlExtras.storedata(eval_metricsArray[:,i+2]; filename=dir*"/latin_$(N)_$(p)_random$(i)")
+    if save_experiment_artefacts
+        save_experiment_weights(agent, directory)
     end
 
-    return metricsArray, eval_metricsArray
+    return metrics_array, eval_metrics_array
 end
-
-
-
-# -------------------
-# -------------------
-
-metricsArray, eval_metricsArray = trytrain(nbEpisodes)
-nothing
+# if abspath(PROGRAM_FILE) == @__FILE__
+metrics_array, eval_metrics_array = train_latin_agent(agent, latin_exp_config, value_selection_array, variable_selection, false)
+# end
