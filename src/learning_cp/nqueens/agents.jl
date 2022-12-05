ef7d025301238a2473aff379cf3b309226a204fe
Léo Boisvert <leo.boisvert@hotmail.ca>
2022-12-05 12:57:49 -0500

    Changed input type for NN model-related functions

--- src/learning_cp/nqueens/agents.jl
+++ src/learning_cp/nqueens/agents.jl
@@ -1,7 +1,7 @@
 include("model_config.jl")
 
 
-function build_nqueens_model(model_config::ModelConfig)
+function build_nqueens_model(model_config::NQueensModelConfig)
     model = SeaPearl.CPNN(
         graphChain=Flux.Chain(
             SeaPearl.GraphConv(model_config.num_input_features => 32, Flux.leakyrelu),
@@ -24,7 +24,7 @@ function build_nqueens_model(model_config::ModelConfig)
     return model
 end
 
-function build_nqueens_agent(agent_config::AgentConfig)
+function build_nqueens_agent(agent_config::NQueensAgentConfig)
     agent = RL.Agent(
         policy=RL.QBasedPolicy(
             learner=RL.DQNLearner(
