bfe6f77541da9ed273c7db536402b12974f6b563
Léo Boisvert <leo.boisvert@hotmail.ca>
2022-12-07 19:10:19 -0500

    Minor cleanup of tsptw agents

--- src/learning_cp/tsptw/agents.jl
+++ src/learning_cp/tsptw/agents.jl
@@ -1,62 +1,64 @@
-agent = RL.Agent(
-    policy = RL.QBasedPolicy(
-        learner = RL.DQNLearner(
-            approximator = RL.NeuralNetworkApproximator(
-                model = SeaPearl.VariableOutputCPNN(
-                    graphChain = Flux.Chain(
-                        SeaPearl.EdgeFtLayer(numInFeatures => 32, 1 => 4),
-                        SeaPearl.EdgeFtLayer(32 => 32,  4 => 4),
-                        SeaPearl.EdgeFtLayer(32 => 32,  4 => 4),
-                        SeaPearl.EdgeFtLayer(32 => 32,  4 => 4),
+function build_tsptw_agent(num_input_features::Int)
+    agent = RL.Agent(
+        policy = RL.QBasedPolicy(
+            learner = RL.DQNLearner(
+                approximator = RL.NeuralNetworkApproximator(
+                    model = SeaPearl.VariableOutputCPNN(
+                        graphChain = Flux.Chain(
+                            SeaPearl.EdgeFtLayer(num_input_features => 32, 1 => 4),
+                            SeaPearl.EdgeFtLayer(32 => 32,  4 => 4),
+                            SeaPearl.EdgeFtLayer(32 => 32,  4 => 4),
+                            SeaPearl.EdgeFtLayer(32 => 32,  4 => 4),
 
+                        ),
+                        nodeChain = Flux.Chain(
+                            Flux.Dense(32, 32, relu),
+                            Flux.Dense(32, 32, relu),
+                        ),
+                        outputChain = Flux.Dense(64, 1),
                     ),
-                    nodeChain = Flux.Chain(
-                        Flux.Dense(32, 32, relu),
-                        Flux.Dense(32, 32, relu),
-                    ),
-                    outputChain = Flux.Dense(64, 1),
+                    optimizer = ADAM(0.0001f0)
                 ),
-                optimizer = ADAM(0.0001f0)
-            ),
-            target_approximator = RL.NeuralNetworkApproximator(
-                model = SeaPearl.VariableOutputCPNN(
-                    graphChain = Flux.Chain(
-                        SeaPearl.EdgeFtLayer(numInFeatures => 32, 1 => 4),
-                        SeaPearl.EdgeFtLayer(32 => 32, 4 => 4),
-                        SeaPearl.EdgeFtLayer(32 => 32, 4 => 4),
-                        SeaPearl.EdgeFtLayer(32 => 32, 4 => 4),
+                target_approximator = RL.NeuralNetworkApproximator(
+                    model = SeaPearl.VariableOutputCPNN(
+                        graphChain = Flux.Chain(
+                            SeaPearl.EdgeFtLayer(num_input_features => 32, 1 => 4),
+                            SeaPearl.EdgeFtLayer(32 => 32, 4 => 4),
+                            SeaPearl.EdgeFtLayer(32 => 32, 4 => 4),
+                            SeaPearl.EdgeFtLayer(32 => 32, 4 => 4),
+                        ),
+                        nodeChain = Flux.Chain(
+                            Flux.Dense(32, 32, relu),
+                            Flux.Dense(32, 32, relu),
+                        ),
+                        outputChain = Flux.Dense(64, 1),
                     ),
-                    nodeChain = Flux.Chain(
-                        Flux.Dense(32, 32, relu),
-                        Flux.Dense(32, 32, relu),
-                    ),
-                    outputChain = Flux.Dense(64, 1),
+                    optimizer = ADAM(0.0001f0)
                 ),
-                optimizer = ADAM(0.0001f0)
+                loss_func = Flux.Losses.huber_loss,
+                stack_size = nothing,
+                γ = 0.99f0,
+                batch_size = 1,
+                update_horizon = 1,
+                min_replay_history = 1,
+                update_freq = 1,
+                target_update_freq = 100,
             ),
-            loss_func = Flux.Losses.huber_loss,
-            stack_size = nothing,
-            γ = 0.99f0,
-            batch_size = 1,#32,
-            update_horizon = 1,
-            min_replay_history = 1,
-            update_freq = 1,
-            target_update_freq = 100,
+            explorer = RL.EpsilonGreedyExplorer(
+                ϵ_stable = 0.01,
+                kind = :exp,
+                ϵ_init = 1.0,
+                warmup_steps = 0,
+                decay_steps = 2000,
+                step = 1,
+                is_break_tie = false,
+                rng = MersenneTwister(33)
+            )
         ),
-        explorer = RL.EpsilonGreedyExplorer(
-            ϵ_stable = 0.01,
-            kind = :exp,
-            ϵ_init = 1.0,
-            warmup_steps = 0,
-            decay_steps = 2000,
-            step = 1,
-            is_break_tie = false,
-            #is_training = true,
-            rng = MersenneTwister(33)
+        trajectory = RL.CircularArraySARTTrajectory(
+            capacity = 4000,
+            state = SeaPearl.TsptwTrajectoryState[] => ()
         )
-    ),
-    trajectory = RL.CircularArraySARTTrajectory(
-        capacity = 4000,
-        state = SeaPearl.TsptwTrajectoryState[] => ()
     )
-)
+    return agent
+end
