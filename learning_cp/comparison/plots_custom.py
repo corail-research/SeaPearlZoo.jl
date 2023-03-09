413c9f902ff4c320eb0762e9b8a7e07b754beeca
3rdCore <tom.marty@polytec>
2023-03-09 10:51:59 -0500

    added custom plot func

--- learning_cp/comparison/plots_custom.py
+++ learning_cp/comparison/plots_custom.py
@@ -6,17 +6,11 @@ import numpy as np
 import pandas as pd
 import os
 import re
-from IPython.display import display
 import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
 import seaborn as sns
-pd.options.display.float_format = '{:,.4f}'.format
 import importlib
-import sys
-sys.path.insert(0, "/home/martom/SeaPearl/SeaPearlExtras.jl/src/metrics/basicmetrics/")
-import plots
-import benchmarkPy
+from IPython.display import display
+pd.options.display.float_format = '{:,.4f}'.format
 
 def modify_and_import(module_name, package, modification_func):
     spec = importlib.util.find_spec(module_name, package)
@@ -28,12 +22,10 @@ def modify_and_import(module_name, package, modification_func):
     sys.modules[module_name] = module
     return module
 
-path = "/home/martom/SeaPearl/SeaPearlZoo/learning_cp/comparison/experiment_for_paper/Score vs General/Merged_results/MIS50/"
+#path = "/home/martom/SeaPearl/SeaPearlZoo/learning_cp/comparison/experiment_for_paper/Score vs General/Merged_results/MC50/"
+path = "experiment_for_paper/Final_Benchmark/MIS/L/exp_012_maxwell03_MIS_final_benchmark_M_100_100_10000_161_12-23-18_AAA"
 
 plots = modify_and_import("plots", None, lambda src: src.replace("hello", "world"))
 
-plots.all(path, window=100, estimator=np.mean, ilds= False)
-
-path ="/home/martom/SeaPearl/SeaPearlZoo/learning_cp/benchmarks/Experience 2/"
-
-benchmark.all(path)
+#plots.all(path +"/", window=100, estimator=np.mean, ilds= False)
+plots.performance(path +"/", ilds= eval)
