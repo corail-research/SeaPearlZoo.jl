a8b4d1e332ac04e6b855c77b784b236b41eed224
Max Bourgeat <max.bourgeat@ensta-bretagne.org>
2023-06-28 16:27:32 -0400

    Add heterogeneous state representation for graph coloring

---
+++ src/learning_cp/graph_coloring/dqn_hsr/heterogeneous_coloring_model.jl
@@ -0,0 +1,146 @@
+# Model definition
+
+struct HeterogeneousModel{A,B}
+    Inputlayer::A
+    Middlelayers::Vector{B}
+
+    function HeterogeneousModel(Inputlayer, Middlelayers)
+        return new{typeof(Inputlayer), eltype(Middlelayers)}(Inputlayer,Middlelayers)
+    end
+
+    function HeterogeneousModel(original_features_size::Vector{Int}, mid::Int, out::Int, n_layers::Int; pool=SeaPearl.meanPooling(), init = Flux.glorot_uniform)
+    
+    Middlelayers=SeaPearl.HeterogeneousGraphConv[]
+
+    if n_layers == 1 
+        Inputlayer = get_heterogeneous_graph_conv_init_layer(original_features_size, out, init = init)
+    else
+        Inputlayer = get_heterogeneous_graph_conv_init_layer(original_features_size, mid, init = init)
+        for i in 1:n_layers - 2
+            push!(Middlelayers, get_heterogeneous_graph_conv_layer(mid, mid, original_features_size, pool, init = init))
+        end
+        push!(Middlelayers,get_heterogeneous_graph_conv_layer(mid, out, original_features_size, pool, init = init))
+    end
+    return new{typeof(Inputlayer), eltype(Middlelayers)}(Inputlayer, Middlelayers)
+
+    end
+end
+
+Flux.@functor HeterogeneousModel
+"""
+function Flux.functor(::Type{<:HeterogeneousModel}, m)
+    return (m.Inputlayer, m.Middlelayers), ls -> HeterogeneousModel(ls[1], ls[2])
+end
+"""
+function (m::HeterogeneousModel)(fg)
+    original_fg = deepcopy(fg)
+    out = m.Inputlayer(fg)
+    for layer in m.Middlelayers
+        out = layer(out, original_fg)
+    end
+    return out
+end
+
+get_heterogeneous_graph_conv_layer(in, out, original_features_size, pool; init = Flux.glorot_uniform) = SeaPearl.HeterogeneousGraphConv(in => out, original_features_size, Flux.leakyrelu; pool = pool, init = init)
+
+get_heterogeneous_graph_conv_init_layer(original_features_size, out; init = Flux.glorot_uniform) = SeaPearl.HeterogeneousGraphConvInit(original_features_size, out, Flux.leakyrelu, init = init)
+
+function get_dense_chain(in, mid, out, n_layers, σ=Flux.identity; init = Flux.glorot_uniform )
+    @assert n_layers >= 1
+    layers = []
+    if n_layers == 1
+        push!(layers, Flux.Dense(in, out, init= init))
+    elseif n_layers == 2
+        push!(layers, Flux.Dense(in, mid, σ, init= init))
+        push!(layers, Flux.Dense(mid, out, init= init))
+    else
+        push!(layers, Flux.Dense(in, mid, σ, init= init))
+        for i in 2:(n_layers-1)
+            push!(layers, Flux.Dense(mid, mid, σ, init= init))
+        end
+        push!(layers, Flux.Dense(mid, out, init= init))
+    end
+    return Flux.Chain(layers...)
+end
+
+function get_heterogeneous_agent(; get_explorer, batch_size=16, update_horizon, min_replay_history, update_freq=1, target_update_freq=200, γ = 0.999f0, get_heterogeneous_trajectory, get_heterogeneous_nn)
+    return RL.Agent(
+        policy=RL.QBasedPolicy(
+            learner=get_heterogeneous_learner(batch_size, update_horizon, min_replay_history, update_freq, target_update_freq, get_heterogeneous_nn, γ),
+            explorer=get_explorer(),
+        ),
+        trajectory=get_heterogeneous_trajectory()
+    )
+end
+
+function get_heterogeneous_learner(batch_size, update_horizon, min_replay_history, update_freq, target_update_freq, get_heterogeneous_nn, γ)
+    return RL.DQNLearner(
+        approximator=RL.NeuralNetworkApproximator(
+            model=get_heterogeneous_nn(),
+            optimizer=ADAM()
+        ),
+        target_approximator=RL.NeuralNetworkApproximator(
+            model=get_heterogeneous_nn(),
+            optimizer=ADAM()
+        ),
+        loss_func=Flux.Losses.huber_loss,
+        batch_size=batch_size,
+        update_horizon=update_horizon,
+        min_replay_history=min_replay_history,
+        update_freq=update_freq,
+        target_update_freq=target_update_freq,
+        γ = γ
+    )
+end
+
+function get_heterogeneous_slart_trajectory(; capacity, n_actions)
+    return RL.CircularArraySLARTTrajectory(
+        capacity=capacity,
+        state=SeaPearl.HeterogeneousTrajectoryState[] => (),
+        legal_actions_mask=Vector{Bool} => (n_actions,),
+    )
+end
+
+function get_epsilon_greedy_explorer(decay_steps, ϵ_stable; rng=nothing)
+    if isnothing(rng)
+        return RL.EpsilonGreedyExplorer(
+            ϵ_stable=ϵ_stable,
+            kind=:exp,
+            decay_steps=decay_steps,
+            step=1
+        )
+    else
+        return RL.EpsilonGreedyExplorer(
+            ϵ_stable=ϵ_stable,
+            kind=:exp,
+            decay_steps=decay_steps,
+            step=1,
+            rng = rng
+        )
+    end
+end
+
+function get_heterogeneous_graph_chain(original_features_size, mid, out, n_layers; pool=SeaPearl.meanPooling(), init = Flux.glorot_uniform, device = cpu)
+    @assert n_layers >= 1
+    return HeterogeneousModel(original_features_size, mid, out, n_layers; pool= pool, init = init)
+end
+
+function get_heterogeneous_fullfeaturedcpnn(;feature_size, conv_type="gc", conv_size=8, dense_size=16, output_size=1, n_layers_graph=3, n_layers_node=2, n_layers_output=2, pool=SeaPearl.meanPooling(), σ=Flux.leakyrelu, heads=4, init = Flux.glorot_uniform, device = cpu)
+    if conv_type == "gc"
+        return SeaPearl.HeterogeneousFullFeaturedCPNN(
+            get_heterogeneous_graph_chain(feature_size, conv_size, conv_size, n_layers_graph; pool=pool, init = init),
+            get_dense_chain(conv_size, dense_size, dense_size, n_layers_node, σ, init = init),
+            Flux.Chain(),
+            get_dense_chain(2*dense_size, dense_size, output_size, n_layers_output, σ, init = init)
+            )|> device
+    elseif conv_type == "hgt"
+        return SeaPearl.HeterogeneousFullFeaturedCPNN(
+            get_hgt(feature_size, conv_size, n_layers_graph; heads=heads, init = init),
+            get_dense_chain(conv_size, dense_size, dense_size, n_layers_node, σ, init = init),
+            Flux.Chain(),
+            get_dense_chain(2*dense_size, dense_size, output_size, n_layers_output, σ, init = init)
+        )
+    else
+        error("conv_type unknown!")
+    end
+end
---
+++ src/learning_cp/graph_coloring/dqn_hsr/heterogeneous_graphe_coloring.jl
@@ -0,0 +1,106 @@
+using Flux
+using LightGraphs
+using Random
+using ReinforcementLearning
+const RL = ReinforcementLearning
+using SeaPearl
+
+include("../coloring_config.jl")
+include("heterogeneous_coloring_model.jl")
+include("../coloring_pipeline.jl")
+include("argparse_heterogeneous_graph_coloring.jl")
+include("../../utils/save_metrics.jl")
+
+coloring_settings, instance_generator, csv_path = set_settings()
+
+function select_random_value(x::SeaPearl.IntVar; cpmodel=nothing)
+    selected_number = rand(1:length(x.domain))
+    i = 1
+    for value in x.domain
+        if i == selected_number
+            return value
+        end
+        i += 1
+    end
+    @assert false "This should not happen"
+end
+
+
+reward = SeaPearl.GeneralReward
+SR_ffcpnn = SeaPearl.HeterogeneousStateRepresentation{SeaPearl.DefaultFeaturization,SeaPearl.HeterogeneousTrajectoryState}
+
+# -------------------
+# Value Heuristic definition
+# -------------------
+output_size = instance_generator.n
+agent_config = ColoringAgentConfig(0.99f0, 32, output_size, 4, 400, 1, 350, 3000)
+
+n_nodes = coloring_settings.nbNodes
+seedEval = 42
+n_step_per_episode = n_nodes
+update_horizon = Int(round(n_step_per_episode//2))
+
+evalFreq=coloring_settings.evalFreq
+
+feature_size = [6, 5, 2]
+
+decay_steps = Int(floor(coloring_settings.nbEpisodes*coloring_settings.restartPerInstances*(n_nodes+1)*0.5))
+
+rngExp = MersenneTwister(seedEval)
+init = Flux.glorot_uniform(MersenneTwister(seedEval))
+pool = SeaPearl.sumPooling()
+trajectory_capacity=10000
+
+chosen_features = Dict(
+    "node_number_of_neighbors" => true,
+    "constraint_type" => true,
+    "constraint_activity" => true,
+    "nb_not_bounded_variable" => true,
+    "variable_initial_domain_size" => true,
+    "variable_domain_size" => true,
+    "variable_is_objective" => true,
+    "variable_assigned_value" => true,
+    "variable_is_bound" => true,
+    "values_raw" => true)
+
+agent_ffcpnn = get_heterogeneous_agent(;
+    get_heterogeneous_trajectory = () -> get_heterogeneous_slart_trajectory(capacity=trajectory_capacity, n_actions=n_nodes),        
+    get_explorer = () -> get_epsilon_greedy_explorer(decay_steps, 0.05; rng = rngExp ),
+    batch_size=32,
+    update_horizon=update_horizon,
+    min_replay_history=Int(round(16*n_step_per_episode//2)),
+    update_freq=1,
+    target_update_freq=7*n_step_per_episode,
+    get_heterogeneous_nn = () -> get_heterogeneous_fullfeaturedcpnn(
+        feature_size=feature_size,
+        conv_size=8,
+        dense_size=16,
+        output_size=1,
+        n_layers_graph=3,
+        n_layers_node=2,
+        n_layers_output=2, 
+        pool=pool,
+        σ=NNlib.leakyrelu,
+        init = init
+    ),
+    γ = 0.99f0
+    )
+
+learned_heuristic_ffcpnn = SeaPearl.SimpleLearnedHeuristic{SR_ffcpnn,reward,SeaPearl.FixedOutput}(agent_ffcpnn, chosen_features=chosen_features)
+
+# Basic value-selection heuristic
+selectMin(x::SeaPearl.IntVar; cpmodel=nothing) = SeaPearl.minimum(x.domain)
+heuristic_min = SeaPearl.BasicHeuristic(selectMin)
+
+randomHeuristics = []
+for i in 1:coloring_settings.nbRandomHeuristics
+    push!(randomHeuristics, SeaPearl.BasicHeuristic(select_random_value))
+end
+
+valueSelectionArray = [learned_heuristic_ffcpnn, heuristic_min]
+append!(valueSelectionArray, randomHeuristics)
+variableSelection = SeaPearl.MinDomainVariableSelection{false}() # Variable Heuristic definition
+
+metricsArray, eval_metricsArray = solve_learning_coloring(agent_ffcpnn, agent_config, coloring_settings, instance_generator)
+
+save_metrics(eval_metricsArray, csv_path)
