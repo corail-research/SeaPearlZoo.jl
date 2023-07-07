3361c5fdc3137735685a1acca4274fec952779ed
Léo Boisvert <leo.boisvert@hotmail.ca>
2023-07-07 15:43:59 -0400

    learning MIS update

--- src/learning_cp/utils/save_metrics.jl
+++ src/learning_cp/utils/save_metrics.jl
@@ -19,8 +19,6 @@ function get_last_solution(solutions::Vector{Union{Nothing, Float32}})
     return nothing
 end
 
-
-
 function save_metrics(eval_metricsArray::Matrix{SeaPearl.AbstractMetrics}, save_path::AbstractString)
 
     column_names = ["num_heuristic", "num_instance", "num_experiment", "heuristic_type", "reward_type", "policy_type", "first_sol", "last_sol", "node_visited_first_sol", "total_node_visited", "total_time"]
@@ -73,3 +71,49 @@ function save_metrics(eval_metricsArray::Matrix{SeaPearl.AbstractMetrics}, save_
     end
 end
 
+function get_metrics_dataframe(eval_metricsArray::Matrix{SeaPearl.AbstractMetrics})::DataFrame
+    rows = NamedTuple{(:num_heuristic, :num_instance, :num_experiment, :heuristic_type, :reward_type, :policy_type, :first_sol, :last_sol, :node_visited_first_sol, :total_node_visited, :total_time)}[]
+    nb_instance, nb_heuristic = size(eval_metricsArray)
+    nb_experiment = length(eval_metricsArray[1, 1].scores)
+
+    for j = 1:nb_heuristic
+        num_heuristic = j
+        heuristic = eval_metricsArray[1, j].heuristic
+        heuristic_type = nothing
+        policy_type = nothing
+        reward_type = nothing
+        if isa(heuristic, SeaPearl.SimpleLearnedHeuristic)
+            if isa(heuristic.agent.policy, PPOPolicy)
+                policy_type = "PPOPolicy"
+            end
+            if isa(heuristic.agent.policy, QBasedPolicy)
+                policy_type = "QBasedPolicy"
+            end
+            reward_type = split(split(string(eval_metricsArray[1, j].heuristic.reward), "(")[1], ".")[2]
+            heuristic_type = "SimpleLearnedHeuristic"
+        end
+        if isa(heuristic, SeaPearl.BasicHeuristic)
+            heuristic_type = "BasicHeuristic(" * string(heuristic.selectValue) * ")"
+        end
+        for i = 1:nb_instance
+            num_instance = i
+            for k = 1:nb_experiment
+                num_experiment = k
+                first_sol = get_first_solution(eval_metricsArray[i, j].scores[k])
+                last_sol = get_last_solution(eval_metricsArray[i, j].scores[k])
+                node_visited_first_sol = eval_metricsArray[i, j].meanNodeVisitedUntilfirstSolFound[k]
+                total_node_visited = eval_metricsArray[i, j].meanNodeVisitedUntilEnd[k]
+                total_time = eval_metricsArray[i, j].TotalTimeNeeded[k]
+
+                # Create the new named tuple to add
+                new_row = (num_heuristic=num_heuristic, num_instance=num_instance, num_experiment=num_experiment, heuristic_type=heuristic_type, reward_type=reward_type, policy_type=policy_type, first_sol=first_sol, last_sol=last_sol, node_visited_first_sol=node_visited_first_sol, total_node_visited=total_node_visited, total_time=total_time)
+
+                # Add the new named tuple to the list of rows
+                push!(rows, new_row)
+            end
+        end
+    end
+
+    # Convert the list of named tuples to a DataFrame
+    return DataFrame(rows)
+end
